a1fd2dcbba2302492952ffeac80a1817b6596b1a
Dekr0 <dekr0.dk@gmail.com>
2020-08-08 12:23:20 -0600

    changed format of code: the sequence of initializing the constant

--- Python/urlextractor/config/config.py
+++ Python/urlextractor/config/config.py
@@ -6,15 +6,18 @@ import os
 from fake_useragent import UserAgent
 
 APPDATA = os.path.join(os.path.dirname(os.path.dirname(__file__)), "appdata")
+REPORT_DIR = os.path.join(APPDATA, r"reports\{}.pdf")
+DATABASE = os.path.join(APPDATA, "ReportURLData.mdb")
+
+BASE_URL = r"http://stockdata.stock.hexun.com{}"
+INIT_REQUEST = BASE_URL.format(r"/2009_ggqw_{}.shtml")
+
 ADOCONNECTION = r"ADODB.Connection"
 ADODBCOMMAND = r"ADODB.Command"
-BASE_URL = "http://stockdata.stock.hexun.com{}"
-DATABASE = os.path.join(APPDATA, "ReportURLData.mdb")
 DSN = "PROVIDER=Microsoft.Jet.OLEDB.4.0;DATA SOURCE={};".format(DATABASE)
-INIT_REQUEST = BASE_URL.format("/2009_ggqw_{}.shtml")
+
 PARSER = "lxml"
-SRC = os.path.join(APPDATA, "temp\\{}.txt")
+
 TITLE_PATTERN = r"\d{{{}}}年({}|{}|{}|{})报告(?!摘要|补充公告|（?正文）?)(?:（?全文）?)?"
+
 USERAGENT = UserAgent().random
-
-
