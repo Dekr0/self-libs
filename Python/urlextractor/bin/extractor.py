3a64081f211d50b6ab4d275336bef1d3004d00e4
Dekr0 <dekr0.dk@gmail.com>
2020-08-08 14:44:48 -0600

    change logging config

--- Python/urlextractor/bin/extractor.py
+++ Python/urlextractor/bin/extractor.py
@@ -8,7 +8,7 @@ import logging
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "libs"))
 from libs import main_extractor
 
-logging.basicConfig(filename="log.log",
+logging.basicConfig(filename="extractor_log.log",
                     format="%(asctime)s - %(levelname)s - %(module)s: %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S %p",
                     level=logging.INFO)  # Setup basic config for a root logger
