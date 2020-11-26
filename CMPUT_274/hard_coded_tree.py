df903bc4acbdaf888e8b71a607001915d60a0dcb
Dekr0 <dekr0.dk@gmail.com>
2020-11-26 14:29:50 -0700

    remove CMPUT-274 folder

--- CMPUT_274/hard_coded_tree.py
+++
@@ -1,7 +0,0 @@
-str = "She sells seashells by the seashore"
-
-huffman = dict()
-huffman[" "] = "000"
-huffman["e"] = "001"
-huffman["S"] = "01000"
-huffman["y"] = "01001"
--- CMPUT_274/test.py
+++
@@ -1,4 +0,0 @@
-def s(n):
-    return n*(n-1)/2
-
-print(s(10))
